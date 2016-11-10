119e7727a6d50816f5ecbbfcbb596a4bf393ecd1
NeverSmileK57CLC <neversmile12b1@gmail.com>
2016-11-10 19:16:47 +0700

    Done get data from github

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,4 +1,18 @@
 class WebhooksController < ApplicationController
   def index
+    action = request.headers["X-GitHub-Event"]
+    case action
+    # when "pull_request_review_comment"
+    #   request = params["comment"]
+    when "pull_request_review"
+      request = params["review"]
+      request_owner = request["user"]
+      pull_owner = params["pull_request"]["user"]
+
+      puts "#{request_owner["login"]} has reviewed on pull of #{pull_owner["login"]}"
+      puts request["html_url"]
+    end
+
+    render json: :nothing
   end
 end
