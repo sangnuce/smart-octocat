04be6e463e84e12e83df99d4afa7d210b7025ad5
NeverSmileK57CLC <neversmile12b1@gmail.com>
2016-11-14 14:44:22 +0700

    Fix bug wrong box

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -12,11 +12,13 @@ class WebhooksController < ApplicationController
     # pull_owner = github_service.get_pull_request_owner
     # body = "[To:#{pull_owner.chatwork_id}] #{pull_owner.name}\n#{action_owner.name} has reviewed on your pull request."
 
-    
+
     # Send message to chatwork
-    body_message = github_service.get_message_to_send
+
+    body_message, room = github_service.get_message_to_send
+
     http_request = HTTP.headers("X-ChatWorkToken" => "256a72efa51cd416b879d729eb794ee2")
-      .post("https://api.chatwork.com/v1/rooms/60833608/messages", params: {body: body_message})
+      .post("https://api.chatwork.com/v1/rooms/#{room.chatwork_id}/messages", params: {body: body_message})
     puts http_request.to_s
 
     render json: :nothing
