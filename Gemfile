d42fe4df553944b0c9f96c7d3ce953f8aa0b346f
NeverSmileK57CLC <neversmile12b1@gmail.com>
2016-11-16 10:25:36 +0700

    deploy to virtualbox using capistrano

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ end
 
 group :development do
   gem "web-console", "~> 2.0"
-
   gem "spring"
+  gem "capistrano"
+  gem "capistrano-bundler"
+  gem "capistrano-rails"
 end
---
+++ config/deploy.rb
@@ -0,0 +1,68 @@
+# config valid only for current version of Capistrano
+lock "3.6.1"
+
+set :application, "smart-octocat"
+set :repo_url, "git@github.com:NeverSmileK57CLC/smart-octocat.git"
+
+
+set :rails_env, "staging"
+
+set :migration_role, :db
+
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, "master"
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deploy/smart-octocat"
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+set :linked_files, fetch(:linked_files, []).push(
+  "config/local_env.yml",
+  "config/database.yml",
+  "config/secrets.yml",
+)
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+set :linked_dirs, fetch(:linked_dirs, []).push(
+  "bin",
+  "log",
+  "tmp/pids",
+  "tmp/cache",
+  "tmp/sockets",
+  "vendor/bundle",
+  "public/system",
+)
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+namespace :deploy do
+  desc "Restart application"
+  task :restart do
+    on roles(:app), in: :requence, wait: 5 do
+      execute :touch, release_path.join("tmp/restart.txt")
+    end
+  end
+
+  after :publishing, "deploy:restart"
+  after :finishing, "deploy:cleanup"
+end
