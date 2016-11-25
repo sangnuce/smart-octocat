81e80c705f4d10ea2716988d4af9275877f7d45d
NeverSmileK57CLC <neversmile12b1@gmail.com>
2016-11-25 09:11:51 +0700

    Fix send messages when deployed

--- app/controllers/deployhooks_controller.rb
+++ app/controllers/deployhooks_controller.rb
@@ -5,23 +5,15 @@ class DeployhooksController < ApplicationController
     to_part = qas.map{|qa| "[To:#{qa.chatwork_id}]#{qa.name}"}
     to_part = to_part.join + "\n"
 
-    # not_deployed_pulls = PullRequest.not_deployed
-    not_deployed_pull = PullRequest.not_deployed.last
+    not_deployed_pulls = PullRequest.filter_by_room(room.id).not_deployed
 
-    # message = ""
-    # not_deployed_pulls.each_with_index do |pull, index|
-    #   title = pull.title || ""
-    #   description = pull.description || ""
-    #   pull.update deployed: true
-    #   message += "#{index + 1}. " + title + "\n"
-    #   message += description + "\n"
-    # end
-
-    if not_deployed_pull.present?
-      not_deployed_pull.update deployed: true
-      body = "#{to_part}[info][title]#{not_deployed_pull.title}[/title]" +
-        "#{not_deployed_pull.description || "Nothing to say."}[/info]"
-
+    if not_deployed_pulls.present?
+      body = to_part
+      not_deployed_pulls.each do |pull|
+        pull.update deployed: true
+        body += "[info][title]#{pull.title}[/title]" +
+          "#{pull.description.presence || "Nothing to say."}[/info]"
+      end
       http_request = HTTP.headers("X-ChatWorkToken" => ENV["chatwork_secret"])
         .post("https://api.chatwork.com/v1/rooms/#{room.chatwork_id}/messages", params: {body: body})
       puts http_request.to_s
--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -4,4 +4,8 @@ class PullRequest < ApplicationRecord
   belongs_to :merged_by, class_name: "User", foreign_key: "merged_by_id"
 
   scope :not_deployed, ->{where "(deployed is ? OR deployed = ?) AND merged = ?", nil, false, true}
+
+  scope :filter_by_room, ->room_id do
+    joins(:user_room).where "user_rooms.room_id = ?", room_id
+  end
 end
